538bd4f7200cd6ac04acec9fa6fb7327830f5511
Renato Vilaça <renatovilaca@gmail.com>
2021-06-04 16:13:31 -0300

    Rich domain entities

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,37 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Too short, minimum 3 characters.");
+
+            Name = name;
+        }
     }
 }
