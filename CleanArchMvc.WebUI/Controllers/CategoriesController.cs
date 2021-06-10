55ae9fd07f311be82c37927a048db0783f45e5a4
Renato Vilaça <renatovilaca@gmail.com>
2021-06-09 23:35:05 -0300

    Initial creation for controllers and views

---
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        //DI
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetCategories();
+            return View(categories);
+        }
+    }
+}
